56b19e1e9172f0ca9b61d7356d1c3c48346f1520
hobiJeong <jjb26433@naver.com>
2024-12-21 20:10:14 +0900

    refactor/#15: Modify swagger title, description

--- src/bootstrap.service.ts
+++ src/bootstrap.service.ts
@@ -114,9 +114,9 @@ export class BootstrapService {
     const YAML_PATH = 'api-docs-yaml';
 
     const config = new DocumentBuilder()
-      .setTitle('NestJS Boilerplate')
+      .setTitle('Honey Moa Server')
       .setDescription(
-        'NestJS Boilerplate API</br>' +
+        'Honey Moa Server API</br>' +
           `<a target="_black" href="${DOMAIN}/${JSON_PATH}">json document</a></br>` +
           `<a target="_black" href="${DOMAIN}/${YAML_PATH}">yaml document</a></br>`,
       )
