48429c63f0e94930f0b6c5b94a89e1550ffcaa75
hobiJeong <jjb26433@naver.com>
2025-02-03 18:30:08 +0900

    doc/#92: findOneBlogPost swagger 문서 작성

--- src/features/blog-post/controllers/blog-post.swagger.ts
+++ src/features/blog-post/controllers/blog-post.swagger.ts
@@ -157,84 +157,13 @@ export const ApiBlogPost: ApiOperator<keyof BlogPostController> = {
       HttpBadRequestException.swaggerBuilder(HttpStatus.BAD_REQUEST, [
         {
           code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: 'tagNames가 중복됨.',
+          description: 'blogPost의 id가 numeric string이 아님.',
           additionalErrors: {
             errors: [
               {
-                property: 'tagNames',
-                value: ['abc', 'abc'],
-                reason: "All tagNames's elements must be unique",
-              },
-            ],
-            errorType: CustomValidationError,
-          },
-        },
-        {
-          code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: 'tagNames의 요소가 1 이상 20 이하가 아님',
-          additionalErrors: {
-            errors: [
-              {
-                property: 'tagNames',
-                value: ['', 'abc'],
-                reason:
-                  'each value in tagNames must be longer than or equal to 1 and shorter than or equal to 20 characters',
-              },
-            ],
-            errorType: CustomValidationError,
-          },
-        },
-        {
-          code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: 'location이 1 이상 100 이하가 아님',
-          additionalErrors: {
-            errors: [
-              {
-                property: 'location',
-                value: '',
-                reason: 'location must be longer than or equal to 1 characters',
-              },
-            ],
-            errorType: CustomValidationError,
-          },
-        },
-        {
-          code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: 'title이 1 이상 255 이하가 아님',
-          additionalErrors: {
-            errors: [
-              {
-                property: 'title',
-                value: '',
-                reason: 'title must be longer than or equal to 1 characters',
-              },
-            ],
-            errorType: CustomValidationError,
-          },
-        },
-        {
-          code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: 'date가 date format이 아님',
-          additionalErrors: {
-            errors: [
-              {
-                property: 'date',
-                value: '2025-0122',
-                reason: 'date must be a valid ISO 8601 date string',
-              },
-            ],
-            errorType: CustomValidationError,
-          },
-        },
-        {
-          code: COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          description: '그 외 기타 등등 많음',
-          additionalErrors: {
-            errors: [
-              {
-                property: 'contents',
-                value: [],
-                reason: 'contents should not be empty',
+                value: '6741371996205169262ㅁㄴㅇㅁㄴㅇ',
+                property: 'id',
+                reason: 'param internal the id must be a numeric string',
               },
             ],
             errorType: CustomValidationError,
@@ -249,14 +178,15 @@ export const ApiBlogPost: ApiOperator<keyof BlogPostController> = {
       ]),
       HttpForbiddenException.swaggerBuilder(HttpStatus.FORBIDDEN, [
         {
-          code: USER_CONNECTION_ERROR_CODE.YOU_ARE_NOT_PART_OF_A_CONNECTION,
-          description: '해당 blog의 커넥션에 속해있지 않음.',
+          code: COMMON_ERROR_CODE.PERMISSION_DENIED,
+          description:
+            '비공개 게시글의 경우 커넥션에 속해 있지 않으면 403 에러 처리',
         },
       ]),
       HttpNotFoundException.swaggerBuilder(HttpStatus.NOT_FOUND, [
         {
           code: COMMON_ERROR_CODE.RESOURCE_NOT_FOUND,
-          description: '해당 blog가 존재하지 않음.',
+          description: '해당 blogPost가 존재하지 않음.',
         },
       ]),
     );
