9fdfe7ee4153bb5fb30751d6902a6d8320084b20
NicoDora <pjh76574734@gmail.com>
2025-02-05 20:04:04 +0900

    modify/#104: guard module provider에 JwtRefreshTokenAuthGuard 추가

--- src/libs/guards/guard.module.ts
+++ src/libs/guards/guard.module.ts
@@ -3,9 +3,11 @@ import { BasicTokenGuard } from '@libs/guards/providers/basic-auth.guard';
 import { JwtAccessTokenAuthGuard } from '@libs/guards/providers/jwt-access-token-auth.guard';
 import { JwtBearerAuthStrategy } from '@libs/guards/providers/jwt-bearer-auth.strategy';
 import { SocketJwtBearerAuthGuard } from '@libs/guards/providers/socket-jwt-bearer-auth.guard';
+import { JwtRefreshTokenAuthGuard } from '@libs/guards/providers/jwt-refresh-token-auth.guard';
 
 const guards: Provider[] = [
   JwtAccessTokenAuthGuard,
+  JwtRefreshTokenAuthGuard,
   JwtBearerAuthStrategy,
   BasicTokenGuard,
   SocketJwtBearerAuthGuard,
