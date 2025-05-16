d35d49a283ccf3b11a6b3d2996000156f1d60196
hobiJeong <jjb26433@naver.com>
2025-05-16 17:43:52 +0900

    setup/#212: test 환경 셋업

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/$1',
+    '^@features/(.*)$': '<rootDir>/features/$1',
+    '^@config/(.*)$': '<rootDir>/configs/$1',
+    '^@libs/(.*)$': '<rootDir>/libs/$1',
+    '^@tests/(.*)$': '<rootDir>/test/$1',
+  },
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  displayName: 'honey-moa-server',
+  transform: {
+    '^.+.tsx?$': 'ts-jest',
+  },
+  globalSetup: '<rootDir>/../test/global-setup.ts',
+  globalTeardown: '<rootDir>/../test/global-teardown.ts',
+};
